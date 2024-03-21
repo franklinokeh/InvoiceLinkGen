45712bf65154a2b50e8978aafef7234617bbac57
Franklin okeh <franklin.okeh@alerzo.com>
2024-03-21 22:42:38 +0100

    initial commit with dependencies

---
+++ src/main/java/com/ecotech/invoicelinkgen/InvoiceLinkGenApplication.java
@@ -0,0 +1,13 @@
+package com.ecotech.invoicelinkgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceLinkGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceLinkGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecotech/invoicelinkgen/InvoiceLinkGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecotech.invoicelinkgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceLinkGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
